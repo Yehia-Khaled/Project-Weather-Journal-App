6e693c1cd65763acf733e0693149302e9aae012a
Yehia Khaled <55062155+Yehia-Khaled@users.noreply.github.com>
2021-03-22 21:28:12 +0200

    API credentials from OpenWeatherMap website

--- website/app.js
+++ website/app.js
@@ -1,5 +1,6 @@
 /* Global Variables */
-
+const apikey='&appid=78109ed9196112b0a5be3e40a1432a19';
+const baseurl='https://api.openweathermap.org/data/2.5/weather?q=,'
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
