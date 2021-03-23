bd16de43232b5a629ba4f157d77678c903e06429
Yehia Khaled <55062155+Yehia-Khaled@users.noreply.github.com>
2021-03-23 21:24:16 +0200

    get user data when click generate

--- website/app.js
+++ website/app.js
@@ -6,15 +6,16 @@ const unit='&units=metric';
 
 
 /* End Global Variables */
-// Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
 
 //create event listener when click in generate 
 document.getElementById('generate').addEventListener('click',performaction)
 //create perform action function "called with event listener"
 function performaction()
 {
+    // Create a new date instance dynamically with JS
+    let d = new Date();
+    let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
     //get zipcode value when user click generate
     const zipcode=document.getElementById('zip').value;
     //get feelings value when user click generate
