2fdf56f196de747f71f9f1e86e253b4ff2369699
Yehia Khaled <55062155+Yehia-Khaled@users.noreply.github.com>
2021-05-04 23:10:56 +0200

    configuration of heroku

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(cors());
 // Initialize the main project folder
 app.use(express.static('website'));
 
-const port=3030
+const port = process.env.PORT || 3000;
 const server=app.listen(port,listening)
 function listening()
 {
